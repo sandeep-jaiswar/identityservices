0780a0545bbd6dfb7ef1cf11191da2e1d5939086
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2023-05-06 11:00:56 +0530

    fixinf pipeline issue

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { Request, Response } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { UsersService } from '../users/users.service';
-import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
